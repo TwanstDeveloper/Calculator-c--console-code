fcb4102699a8bef024d3bd2dc6f268e4dbcb1458
Twanst Codes <98418359+TwanstDeveloper@users.noreply.github.com>
2023-03-13 19:32:18 +0300

    file

---
+++ Calculation/Calculation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number ");
+            
+            double x=Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter Operation ");
+            string result=Console.ReadLine();
+            Console.Write("Enter Number ");
+
+            double y =Convert.ToDouble(Console.ReadLine());
+
+            if (result=="+")
+            {
+                x += y;
+            }
+            else if (result=="-")
+            {
+                x -= y;
+            }else if (result == "*")
+            {
+                x*=y;
+            }
+            else if (result=="/")
+            {
+                x /= y;
+            }
+            Console.WriteLine("Result= "+x);
+            Console.ReadKey();
+        }
+
+    }
+}
